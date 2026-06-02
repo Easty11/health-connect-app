a3cc6642475c74616712baadb9e9e05d7fa5fedb
Luke <luke@health-app.local>
2026-06-02 15:19:48 +1000

    fix: dedicated PermissionRationaleActivity for HC intent filter, translucent theme

---
+++ android/app/src/main/java/com/anonymous/healthconnectapp/PermissionRationaleActivity.kt
@@ -0,0 +1,11 @@
+package com.anonymous.healthconnectapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class PermissionRationaleActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
